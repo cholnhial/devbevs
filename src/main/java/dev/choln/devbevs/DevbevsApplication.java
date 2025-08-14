8d01cffcfe9050247e9288b737dcef8eb416a0d5
Chol Nhial Chol <chol.chol@autogeneral.com.au>
2025-08-14 12:47:31 +1000

    first commit

---
+++ src/main/java/dev/choln/devbevs/DevbevsApplication.java
@@ -0,0 +1,13 @@
+package dev.choln.devbevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/choln/devbevs/DevbevsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.choln.devbevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
